c4cc27a5ed9d6becc3e97614085bbf6a50108aae
hahagarden <tingyuan413@gmail.com>
2023-04-19 16:54:04 +0900

    feat: Modal done

--- src/components/BareMinimumRequirements/Modal.js
+++ src/components/BareMinimumRequirements/Modal.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 export const ModalContainer = styled.div`
   // TODO : Modal을 구현하는데 전체적으로 필요한 CSS를 구현합니다.
@@ -7,6 +7,13 @@ export const ModalContainer = styled.div`
 
 export const ModalBackdrop = styled.div`
   // TODO : Modal이 떴을 때의 배경을 깔아주는 CSS를 구현합니다.
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: var(--gray-700);
+  opacity: 0.6;
 `;
 
 export const ModalBtn = styled.button`
@@ -21,9 +28,23 @@ export const ModalBtn = styled.button`
 
 export const ModalView = styled.div.attrs((props) => ({
   // attrs 메소드를 이용해서 아래와 같이 div 엘리먼트에 속성을 추가할 수 있습니다.
-  role: 'dialog',
+  role: "dialog",
 }))`
   // TODO : Modal창 CSS를 구현합니다.
+  width: 50%;
+  height: 50%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: var(--coz-orange-600);
+  border-radius: 3rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: var(--white);
+  font-size: 2rem;
 `;
 
 export const Modal = () => {
@@ -31,18 +52,27 @@ export const Modal = () => {
 
   const openModalHandler = () => {
     // TODO : isOpen의 상태를 변경하는 메소드를 구현합니다.
+    setIsOpen((prev) => !prev);
   };
 
   return (
     <>
       <ModalContainer>
         <ModalBtn
-        // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
+          // TODO : 클릭하면 Modal이 열린 상태(isOpen)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
+          onClick={openModalHandler}
         >
-          Open Modal
+          {isOpen ? "Opened!" : "Open Modal"}
           {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때는 ModalBtn의 내부 텍스트가 'Opened!' 로 Modal이 닫힌 상태(isOpen이 false인 상태)일 때는 ModalBtn 의 내부 텍스트가 'Open Modal'이 되도록 구현해야 합니다. */}
         </ModalBtn>
         {/* TODO : 조건부 렌더링을 활용해서 Modal이 열린 상태(isOpen이 true인 상태)일 때만 모달창과 배경이 뜰 수 있게 구현해야 합니다. */}
+        {isOpen ? (
+          <ModalBackdrop onClick={openModalHandler}>
+            <ModalView>HELLO I AM MODAL :)</ModalView>
+          </ModalBackdrop>
+        ) : (
+          ""
+        )}
       </ModalContainer>
     </>
   );
