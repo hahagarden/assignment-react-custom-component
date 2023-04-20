55ca78288eec95de86c25d17baeb82f8a14d72ba
hahagarden <tingyuan413@gmail.com>
2023-04-20 11:37:23 +0900

    feat: complete Toggle

--- src/components/BareMinimumRequirements/Toggle.js
+++ src/components/BareMinimumRequirements/Toggle.js
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import { useState } from "react";
+import styled from "styled-components";
 
 const ToggleContainer = styled.div`
   position: relative;
@@ -8,48 +8,65 @@ const ToggleContainer = styled.div`
   cursor: pointer;
 
   > .toggle-container {
-    width: 50px;
-    height: 24px;
+    width: 60px;
+    height: 32px;
     border-radius: 30px;
     background-color: #8b8b8b;
-    // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+    overflow: hidden;
+
+    > .toggle-inner {
+      display: block;
+      width: 120px;
+      height: 32px;
+      margin-left: -100%;
+      background: linear-gradient(to right, var(--coz-purple-600) 50%, #8b8b8b 50%);
+    }
+
+    &.toggle--checked .toggle-inner {
+      margin-left: 0;
+      transition: margin-left 0.4s;
+    }
   }
 
   > .toggle-circle {
     position: absolute;
-    top: 1px;
-    left: 1px;
+    top: 5px;
+    left: 5px;
     width: 22px;
     height: 22px;
     border-radius: 50%;
     background-color: #ffffff;
-    // TODO : .toggle--checked 클래스가 활성화 되었을 경우의 CSS를 구현합니다.
+
+    &.toggle--checked {
+      left: 32px;
+    }
+
+    transition: left 0.4s;
   }
 `;
 
 const Desc = styled.div`
-  // TODO : 설명 부분의 CSS를 구현합니다.
+  width: 200px;
+  text-align: center;
+  margin: 0 auto;
 `;
 
 export const Toggle = () => {
   const [isOn, setisOn] = useState(false);
 
   const toggleHandler = () => {
-    // TODO : isOn의 상태를 변경하는 메소드를 구현합니다.
+    setisOn((prev) => !prev);
   };
 
   return (
     <>
-      <ToggleContainer
-        // TODO : 클릭하면 토글이 켜진 상태(isOn)를 boolean 타입으로 변경하는 메소드가 실행되어야 합니다.
-      >
-        {/* TODO : 아래에 div 엘리먼트 2개가 있습니다. 각각의 클래스를 'toggle-container', 'toggle-circle' 로 지정하세요. */}
-        {/* TIP : Toggle Switch가 ON인 상태일 경우에만 toggle--checked 클래스를 div 엘리먼트 2개에 모두 추가합니다. 조건부 스타일링을 활용하세요. */}
-        <div />
-        <div />
+      <ToggleContainer onClick={toggleHandler}>
+        <div className={`toggle-container ${isOn ? "toggle--checked" : ""}`}>
+          <div className="toggle-inner" />
+        </div>
+        <div className={`toggle-circle ${isOn ? "toggle--checked" : ""}`} />
       </ToggleContainer>
-      {/* TODO : Desc 컴포넌트를 활용해야 합니다. */}
-      {/* TIP:  Toggle Switch가 ON인 상태일 경우에 Desc 컴포넌트 내부의 텍스트를 'Toggle Switch ON'으로, 그렇지 않은 경우 'Toggle Switch OFF'가 됩니다. 조건부 렌더링을 활용하세요. */}
+      <Desc>Toggle Switch {isOn ? "ON" : "OFF"}</Desc>
     </>
   );
 };
